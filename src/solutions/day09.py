a9e0b1b0e1480a1be704fce3d951bc569dee32f1
matt.hawthorn <matt.hawthorn@trillianthealth.com>
2024-12-09 16:26:09 -0500

    day 9 2024 solution

--- src/solutions/day09.py
+++ src/solutions/day09.py
@@ -1,90 +1,112 @@
-from fractions import Fraction
-from functools import partial, reduce
-from itertools import repeat, takewhile
-from numbers import Rational
-from operator import mul, sub
-from typing import IO, List, Tuple, TypeVar
+from collections import deque
+from itertools import accumulate, chain
+from typing import IO, Iterable, NamedTuple
 
-from util import iterate
-
-N = TypeVar("N", bound=Rational)
-Series = List[int]
-Polynomial = List[N]
-Vector = List[N]
-Matrix = List[Vector[N]]
-LinearSystem = Tuple[Matrix[N], Vector[N]]
+import util
 
 
-def diff(series: Series) -> Series:
-    return [b - a for a, b in zip(series[1:], series)]
+class BlockRange(NamedTuple):
+    start: int
+    width: int
+    empty: bool
+    id: int = -1
+
+    @property
+    def checksum(self) -> int:
+        return (
+            0
+            if self.empty
+            else self.id * (self.start * self.width + self.width * (self.width - 1) // 2)
+        )
 
 
-def multiply(vec: Vector[N], n: N) -> Vector[N]:
-    return list(map(n.__mul__, vec))
+def compactify(
+    data_blocks: list[BlockRange], empty_blocks: list[BlockRange], fragmented: bool
+) -> list[BlockRange]:
+    compactified = []
+    data_blocks = list(data_blocks)
+    empty_blocks = deque(empty_blocks)
+    while data_blocks and empty_blocks:
+        block = data_blocks.pop()
+        while empty_blocks and empty_blocks[-1].start > block.start:
+            empty_blocks.pop()
+        remainder = block.width
+        empties = []
+        while (remainder > 0) and empty_blocks:
+            empty = empty_blocks.popleft()
+            remainder = (remainder - empty.width) if fragmented else (empty.width - block.width)
+            if remainder >= 0:
+                compactified.append(
+                    BlockRange(
+                        empty.start, empty.width if fragmented else block.width, False, block.id
+                    )
+                )
+                if fragmented:
+                    if remainder == 0:
+                        break
+                else:
+                    if remainder > 0:
+                        empty_blocks.appendleft(
+                            BlockRange(empty.start + block.width, remainder, True, empty.id)
+                        )
+                    break
+            else:
+                if fragmented:
+                    extra = empty.width + remainder
+                    compactified.append(BlockRange(empty.start, extra, False, block.id))
+                    empties.append(
+                        BlockRange(empty.start + extra, empty.width - extra, True, empty.id)
+                    )
+                    break
+                else:
+                    remainder = block.width
+                    empties.append(empty)
+        else:
+            if fragmented:
+                # never broke the while loop and no more empty blocks; extra data block at the end
+                if remainder > 0:
+                    compactified.append(BlockRange(block.start, remainder, False, block.id))
+            else:
+                # no empty space found
+                compactified.append(block)
+
+        empty_blocks.extendleft(reversed(empties))
+
+    compactified.extend(data_blocks)
+    return compactified
 
 
-def subtract(vec1: Vector[N], vec2: Vector[N]) -> Vector[N]:
-    return list(map(sub, vec1, vec2))
+def checksum(blocks: Iterable[BlockRange]) -> int:
+    return sum(map(BlockRange.checksum.fget, blocks))
 
 
-def row_reduce(X_y: LinearSystem, col_ix: int, backward: bool = False) -> LinearSystem:
-    X, y = X_y
-    denom = X[col_ix][col_ix]
-    X[col_ix] = new_row = [Fraction(n, denom) for n in X[col_ix]]
-    y[col_ix] = Fraction(y[col_ix], denom)
-    for i in range(col_ix - 1, -1, -1) if backward else range(col_ix + 1, len(X)):
-        multiplier = X[i][col_ix]
-        X[i] = subtract(X[i], multiply(new_row, multiplier))
-        y[i] = y[i] - y[col_ix] * multiplier
-    return X, y
+def parse(input: Iterable[str]) -> tuple[list[BlockRange], list[BlockRange]]:
+    widths = map(int, chain.from_iterable(map(str.strip, input)))
 
+    def next_block(block: BlockRange, width: int) -> BlockRange:
+        return BlockRange(
+            block.start + block.width, width, not block.empty, block.id + (not block.empty)
+        )
 
-def solve_linear(X: Matrix, y: Vector) -> Vector:
-    X_, y_ = reduce(row_reduce, range(len(X)), (X, y))
-    identity, vars = reduce(partial(row_reduce, backward=True), range(len(X) - 1, -1, -1), (X_, y_))
-    return vars
-
-
-def polynomial_degree(series: Series) -> int:
-    nonzero = any
-    diffs = takewhile(nonzero, iterate(diff, series))
-    return sum(1 for _ in diffs)
-
-
-def solve_polynomial(series: Series) -> Polynomial[Fraction]:
-    degree = polynomial_degree(series)
-    matrix: Matrix[Fraction] = list(
-        map(list, (map(n.__pow__, range(degree)) for n in map(Fraction, range(degree))))
+    all_blocks = accumulate(widths, next_block, initial=BlockRange(0, next(widths), False, 0))
+    by_status = util.groupby(
+        lambda block: block.empty, filter(lambda block: block.width > 0, all_blocks)
     )
-    return solve_linear(matrix, list(map(Fraction, series[:degree])))
-
-
-def evaluate(poly: Polynomial[N], n: int) -> N:
-    powers = map(n.__pow__, range(len(poly)))
-    return sum(map(mul, poly, powers))
-
-
-def parse_series(line: str) -> Series:
-    return list(map(int, line.strip().split()))
+    return by_status.get(False, []), by_status.get(True, [])
 
 
 def run(input: IO[str], part_2: bool = True) -> int:
-    series = list(map(parse_series, input))
-    polys = map(solve_polynomial, series)
-    xs = repeat(-1, len(series)) if part_2 else map(len, series)
-    ys = map(evaluate, polys, xs)
-    return int(sum(ys))
+    data_blocks, empty_blocks = parse(input)
+    compactified = compactify(data_blocks, empty_blocks, fragmented=not part_2)
+    return checksum(compactified)
 
 
-_TEST_INPUT = """
-0 3 6 9 12 15
-1 3 6 10 15 21
-10 13 16 21 30 45""".strip()
+_test_input = """2333133121414131402"""
 
 
 def test():
     import io
 
     f = io.StringIO
-    assert run(f(_TEST_INPUT), part_2=False) == 114
-    assert run(f(_TEST_INPUT), part_2=True) == 2
+    util.assert_equal(run(f(_test_input), part_2=False), 1928)
+    util.assert_equal(run(f(_test_input), part_2=True), 2858)
