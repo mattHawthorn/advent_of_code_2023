4c37153ab483d1a64e884a269beced0f9feedd5b
matt.hawthorn <matt.hawthorn@trillianthealth.com>
2024-12-13 18:35:23 -0500

    working and much simpler day 12 2024 solution for both parts

--- src/solutions/day12.py
+++ src/solutions/day12.py
@@ -1,19 +1,9 @@
 from functools import partial
-from itertools import chain, cycle, filterfalse, islice
+from itertools import cycle
 from typing import IO, Iterable
 
 import util
 from util import Grid, GridCoordinates, WeightedDiGraph
-
-
-def parse(input: Iterable[str]) -> tuple[Grid, WeightedDiGraph[GridCoordinates]]:
-    grid = list(map(str.strip, input))
-    graph = util.grid_to_graph(grid, weight_fn=lambda _: 1)
-    value = util.indexer(grid)
-    region_graph = {
-        c1: {c2: 1 for c2 in neighbors if value(c1) == value(c2)} for c1, neighbors in graph.items()
-    }
-    return grid, region_graph
 
 
 def perimeter(graph: WeightedDiGraph[GridCoordinates], component: set[GridCoordinates]):
@@ -24,38 +14,41 @@ def cost1(graph: WeightedDiGraph[GridCoordinates], component: set[GridCoordinate
     return len(component) * perimeter(graph, component)
 
 
-def is_exterior(graph: WeightedDiGraph[GridCoordinates], coords: GridCoordinates):
-    return len(util.neighbors(graph, coords)) < 4
-
-
-def n_corners(component: set[GridCoordinates], is_interior: bool, coords: GridCoordinates) -> int:
-    is_nbr = util.invert(component.__contains__) if is_interior else component.__contains__
-    membership = list(map(is_nbr, util.grid_neighbors(coords)))
-    nbrs = islice(cycle(membership), 1, None)
-    return sum(map((2).__eq__, map(sum, zip(membership, nbrs))))
-
-
-def number_of_sides(graph: WeightedDiGraph[GridCoordinates], component: set[GridCoordinates]):
-    boundary_nodes = list(filter(partial(is_exterior, graph), component))
-    exterior_nodes = set(
-        filterfalse(
-            component.__contains__, chain.from_iterable(map(util.grid_neighbors, boundary_nodes))
-        )
+def n_corners(component: set[GridCoordinates], coords: GridCoordinates) -> int:
+    is_nbr = component.__contains__
+    is_nbr_adj = map(is_nbr, util.grid_neighbors(coords, diag=False))
+    is_nbr_diag = map(is_nbr, util.grid_neighbors(coords, diag=True))
+    return sum(
+        (na1 and na2 and not nd) or (not na1 and not na2)
+        for (na1, na2), nd in zip(util.window(2, cycle(is_nbr_adj)), is_nbr_diag)
     )
-    interior_corners = sum(map(partial(n_corners, component, True), boundary_nodes))
-    exterior_corners = sum(map(partial(n_corners, component, False), exterior_nodes))
-    return interior_corners + exterior_corners
 
 
-def cost2(graph, component: set[GridCoordinates]):
-    return len(component) * number_of_sides(graph, component)
+def number_of_sides(component: set[GridCoordinates]):
+    corners = sum(map(partial(n_corners, component), component))
+    return corners
+
+
+def cost2(component: set[GridCoordinates]):
+    return len(component) * number_of_sides(component)
+
+
+def parse(input: Iterable[str]) -> tuple[Grid, WeightedDiGraph[GridCoordinates]]:
+    grid = list(map(str.strip, input))
+    value = util.indexer(grid)
+    graph = {
+        c1: {c2: 1 for c2 in neighbors if value(c1) == value(c2)}
+        for c1, neighbors in util.grid_to_graph(grid, weight_fn=lambda _: 1).items()
+    }
+    return grid, graph
 
 
 def run(input: IO[str], part_2: bool = True) -> int:
+    global grid
     grid, graph = parse(input)
     components = util.connected_components(graph)
-    cost = cost2 if part_2 else cost1
-    return sum(map(partial(cost, graph), components))
+    cost = cost2 if part_2 else partial(cost1, graph)
+    return sum(map(cost, components))
 
 
 _test_input = """
